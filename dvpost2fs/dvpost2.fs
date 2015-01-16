38746b306e88ec9425dec20bbc0d81bdd744a7ab
FBW <fw@speedpoint.de>
2015-01-16 11:05:36 +0100

    Stand: 12/2014

---
+++ dvpost2fs/dvpost2.fs
@@ -0,0 +1,11 @@
+# Konfigurationsdatei dvpost2.fs konfiguriert Papiergrößen
+# für dvpost2
+# Die Defaultvorgabe wird dadurch überschrieben
+# Syntax: Formnr;Version;Format;Ausrichtung
+#   Version    : Optional kann die Formularversion 2-stellig angegeben werden
+#   Format     : A4, A5, A6
+#   Ausrichtung: P = Portrait (Hochformat), L = Landscape (Querformat)
+#
+266;;A5;L
+83;;A6;L
+171;;A5;L
